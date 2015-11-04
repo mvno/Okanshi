bb16ba2fc8d2d6b91fa1f6614992482a840fa61b
Kim Christensen <kich@mvno.dk>
2015-11-05 00:02:53 +0100

    Perform ILMerge

--- build.fsx
+++ build.fsx
@@ -298,6 +298,31 @@ Target "Release" (fun _ ->
     |> Async.RunSynchronously
 )
 
+Target "ILMerge" (fun _ ->
+    let performMerge primary secondaries mergePath =
+        let outputFilePath = mergePath @@ (filename primary)
+        ILMerge (fun p -> { p with Libraries = secondaries; TargetPlatform = "v4"; Internalize = Internalize; } ) outputFilePath primary
+
+    !! "src/**/*.??proj"
+    |> Seq.map (fun f -> ((fileNameWithoutExt f) + ".dll", "bin" @@ (System.IO.Path.GetFileNameWithoutExtension f)))
+    |> Seq.map (fun (a, f) ->
+        let mergeDir = f @@ "merge"
+        CreateDir mergeDir
+        let primaryAssembly = f @@ a
+        let secondaryAssemblies = !! (f @@ "*.dll")
+        (primaryAssembly, secondaryAssemblies, mergeDir, f))
+    //|> Seq.iter (fun (a, f, m) -> printfn "%A" f)
+    |> Seq.map (fun (a, s, m, f) -> (a, s |> Seq.filter(fun x -> x.EndsWith(a) |> not), m, f))
+    |> Seq.iter (fun (a, s, m, f) ->
+        performMerge a s m
+        CopyTo m [(f @@ ((fileNameWithoutExt a) + ".xml"))]
+        let files = (!! (f @@ "*.*")) |> Seq.toList
+        printfn "%A" files
+        DeleteFiles files
+        !! (m @@ "*.*") |> Seq.iter(fun x -> x |> MoveFile f)
+        DeleteDir m)
+)
+
 Target "BuildPackage" DoNothing
 
 // --------------------------------------------------------------------------------------
@@ -310,6 +335,7 @@ Target "All" DoNothing
   ==> "Build"
   ==> "CopyBinaries"
   ==> "RunTests"
+  ==> "ILMerge"
   ==> "GenerateReferenceDocs"
   ==> "GenerateDocs"
   ==> "All"
