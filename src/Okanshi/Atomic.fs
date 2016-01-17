61f783c01f848ea8e5a0bc8bb533617dd26027cd
Kim Christensen <kich@mvno.dk>
2016-01-17 23:09:42 +0100

    Add wrappers for atomic operations

---
+++ src/Okanshi/Atomic.fs
@@ -0,0 +1,130 @@
+﻿namespace Okanshi
+
+open System
+open System.Threading
+
+/// Perform atomic operations on a type
+type IAtomic<'T> =
+    /// Get the value
+    abstract Get: unit -> 'T
+    /// Sets the value and returns the old value
+    abstract GetAndSet : 'T -> 'T
+    /// Compare and set the value. If the value returned isn't equal the specified original value, it means
+    /// that the value has changed since the last fetch and the new value hasn't been used
+    abstract CompareAndSet: 'T * 'T -> 'T
+    /// Set the value
+    abstract Set: 'T -> unit
+    /// Increment the value by one
+    abstract Increment: unit -> 'T
+    /// Increment the value by the specified amount
+    abstract Increment: 'T -> 'T
+    
+/// Performs atomic operations on a long
+type AtomicLong(intialValue) =
+    let value = ref intialValue
+
+    new () = AtomicLong(0L)
+
+    /// Get the value
+    member __.Get() = Interlocked.Read(value)
+    /// Sets the value and returns the old value
+    member __.GetAndSet(newValue) = Interlocked.Exchange(value, newValue)
+    /// Compare and set the value. If the value returned isn't equal the specified original value, it means
+    /// that the value has changed since the last fetch and the new value hasn't been used
+    member __.CompareAndSet(newValue, originalValue) = Interlocked.CompareExchange(value, newValue, originalValue)
+    /// Set the value
+    member __.Set(newValue) = Interlocked.Exchange(value, newValue) |> ignore
+    /// Increment the value by one
+    member __.Increment() = Interlocked.Increment(value)
+    /// Increment the value by the specified amount
+    member self.Increment(amount) =
+        let rec increment amount =
+            let originalValue = self.Get()
+            let result = self.CompareAndSet(originalValue + amount, originalValue)
+            if result <> originalValue then increment amount
+            else originalValue + amount
+        increment amount
+
+    interface IAtomic<int64> with
+        member self.Get() = self.Get()
+        member self.GetAndSet(newValue) = self.GetAndSet(newValue)
+        member self.CompareAndSet(newValue, originalValue) = self.CompareAndSet(newValue, originalValue)
+        member self.Set(newValue) = self.Set(newValue)
+        member self.Increment() = self.Increment()
+        member self.Increment(amount) = self.Increment(amount)
+
+type AtomicDouble(intialValue) =
+    let value = new AtomicLong(intialValue |> BitConverter.DoubleToInt64Bits)
+
+    new () = AtomicDouble(0.0)
+
+    /// Get the value
+    member __.Get() = value.Get() |> BitConverter.Int64BitsToDouble
+    /// Sets the value and returns the old value
+    member __.GetAndSet(newValue) = newValue |> BitConverter.DoubleToInt64Bits |> value.GetAndSet |> BitConverter.Int64BitsToDouble
+    /// Compare and set the value. If the value returned isn't equal the specified original value, it means
+    /// that the value has changed since the last fetch and the new value hasn't been used
+    member __.CompareAndSet(newValue, originalValue) =
+        let originalLong = BitConverter.DoubleToInt64Bits(originalValue)
+        let newLong = BitConverter.DoubleToInt64Bits(newValue)
+        value.CompareAndSet(newLong, originalLong) |> BitConverter.Int64BitsToDouble
+    /// Set the value
+    member __.Set(newValue) = newValue |> BitConverter.DoubleToInt64Bits |> value.Set |> ignore
+    /// Increment the value by the specified amount
+    member __.Increment(amount) =
+        let rec loop () =
+            let originalLong = value.Get()
+            let originalDouble = originalLong |> BitConverter.Int64BitsToDouble
+            let newLong = originalDouble + amount |> BitConverter.DoubleToInt64Bits
+            if value.CompareAndSet(newLong, originalLong) <> originalLong then loop()
+            else originalDouble + 1.0
+        loop()
+    /// Increment the value by one
+    member self.Increment() = self.Increment(1.0)
+
+    interface IAtomic<double> with
+        member self.Get() = self.Get()
+        member self.GetAndSet(newValue) = self.GetAndSet(newValue)
+        member self.CompareAndSet(newValue, originalValue) = self.CompareAndSet(newValue, originalValue)
+        member self.Set(newValue) = self.Set(newValue)
+        member self.Increment() = self.Increment()
+        member self.Increment(amount) = self.Increment(amount)
+    
+type AtomicDecimal(initialValue) =
+    let mutable value = initialValue
+    let valueLock = new obj()
+
+    new () = AtomicDecimal(0m)
+
+    /// Get the value
+    member __.Get() = lock valueLock (fun () -> value)
+    /// Sets the value and returns the old value
+    member __.GetAndSet(newValue) =
+        lock valueLock (fun () ->
+            let oldValue = value
+            value <- newValue
+            oldValue
+        )
+    /// Compare and set the value. If the value returned isn't equal the specified original value, it means
+    /// that the value has changed since the last fetch and the new value hasn't been used
+    member __.CompareAndSet(newValue, originalValue) =
+        lock valueLock (fun () ->
+            if originalValue = value then value <- newValue
+            originalValue
+        )
+    /// Set the value
+    member __.Set(newValue) = lock valueLock (fun () -> value <- newValue)
+    /// Increment the value by the specified amount
+    member __.Increment(amount) = lock valueLock (fun () ->
+        value <- value + amount
+        value)
+    /// Increment the value by one
+    member self.Increment() = self.Increment(1m)
+
+    interface IAtomic<decimal> with
+        member self.Get() = self.Get()
+        member self.GetAndSet(newValue) = self.GetAndSet(newValue)
+        member self.CompareAndSet(newValue, originalValue) = self.CompareAndSet(newValue, originalValue)
+        member self.Set(newValue) = self.Set(newValue)
+        member self.Increment() = self.Increment()
+        member self.Increment(amount) = self.Increment(amount)
---
+++ tests/Okanshi.Tests/AtomicDoubleTest.cs
@@ -0,0 +1,136 @@
+using FluentAssertions;
+using Xunit;
+
+namespace Okanshi.Test
+{
+	public class AtomicDoubleTest
+	{
+		[Fact]
+		public void Default_value_is_zero()
+		{
+			var atomicDouble = new AtomicDouble();
+
+			var value = atomicDouble.Get();
+
+			value.Should().Be(0);
+		}
+
+		[Theory]
+		[InlineData(1.0)]
+		[InlineData(10.2)]
+		[InlineData(17.22)]
+		[InlineData(1001.01)]
+		public void Default_value_is_correctly_assigned_when_specified_in_constructor(double expectedValue)
+		{
+			var atomicDouble = new AtomicDouble(expectedValue);
+
+			var value = atomicDouble.Get();
+
+			value.Should().Be(expectedValue);
+		}
+
+		[Theory]
+		[InlineData(1.0)]
+		[InlineData(10.2)]
+		[InlineData(17.22)]
+		[InlineData(1001.01)]
+		public void Get_and_set_returns_the_original_value(double originalValue)
+		{
+			var atomicDouble = new AtomicDouble(originalValue);
+
+			var value = atomicDouble.GetAndSet(1000);
+
+			value.Should().Be(originalValue);
+		}
+
+		[Theory]
+		[InlineData(1.0)]
+		[InlineData(10.2)]
+		[InlineData(17.22)]
+		[InlineData(1001.01)]
+		public void Get_and_set_updates_the_value_as_specified(double newValue)
+		{
+			var atomicDouble = new AtomicDouble();
+
+			atomicDouble.GetAndSet(newValue);
+
+			var value = atomicDouble.Get();
+			value.Should().Be(newValue);
+		}
+
+		[Theory]
+		[InlineData(1.0)]
+		[InlineData(10.2)]
+		[InlineData(17.22)]
+		[InlineData(1001.01)]
+		public void Compare_and_set_updates_the_value_when_original_value_is_correct(double newValue)
+		{
+			var atomicDouble = new AtomicDouble();
+
+			atomicDouble.CompareAndSet(newValue, 0);
+
+			var value = atomicDouble.Get();
+			value.Should().Be(newValue);
+		}
+
+		[Theory]
+		[InlineData(1.0)]
+		[InlineData(10.2)]
+		[InlineData(17.22)]
+		[InlineData(1001.01)]
+		public void Compare_and_set_does_not_update_the_value_when_original_value_is_incorrect(double newValue)
+		{
+			var atomicDouble = new AtomicDouble();
+
+			atomicDouble.CompareAndSet(newValue, 100);
+
+			var value = atomicDouble.Get();
+			value.Should().Be(0);
+		}
+
+		[Theory]
+		[InlineData(1.0)]
+		[InlineData(10.2)]
+		[InlineData(17.22)]
+		[InlineData(1001.01)]
+		public void Setting_the_value_updates_the_value_as_expected(double newValue)
+		{
+			var atomicDouble = new AtomicDouble();
+
+			atomicDouble.Set(newValue);
+
+			var value = atomicDouble.Get();
+			value.Should().Be(newValue);
+		}
+
+		[Theory]
+		[InlineData(1.0, 2.0)]
+		[InlineData(10.2, 11.2)]
+		[InlineData(17.22, 18.22)]
+		[InlineData(1001.01, 1002.01)]
+		public void Incrementing_the_value_increments_the_value(double originvalValue, double expectedValue)
+		{
+			var atomicDouble = new AtomicDouble(originvalValue);
+
+			atomicDouble.Increment();
+
+			var value = atomicDouble.Get();
+			value.Should().Be(expectedValue);
+		}
+
+		[Theory]
+		[InlineData(1.0, 2.0, 3.0)]
+		[InlineData(10.2, 11.2, 21.4)]
+		[InlineData(17.22, 18.22, 35.44)]
+		[InlineData(1001.01, 1002.01, 2003.02)]
+		public void Incrementing_the_value_by_the_specified_amount_increments_the_value_correctly(double originvalValue, double amount, double expectedValue)
+		{
+			var atomicDouble = new AtomicDouble(originvalValue);
+
+			atomicDouble.Increment(amount);
+
+			var value = atomicDouble.Get();
+			value.Should().Be(expectedValue);
+		}
+	}
+}
