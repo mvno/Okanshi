2fbb66292a60a4b35d9bf0d3bc2d0ec088d3dbc5
Kim Christensen <kich@mvno.dk>
2016-08-30 01:12:54 +0200

    Expand MonitorKey to contain tags
    This is to enable creating multiple measurements with different tags

--- src/Okanshi/Monitor.fs
+++ src/Okanshi/Monitor.fs
@@ -6,7 +6,7 @@ open System.Threading
 open System.Collections.Concurrent
 
 /// Tuple representing a key used to identify a monitor
-type MonitorKey = (string * Type)
+type MonitorKey = (string * Type * Tag array)
 /// Type representing a monitor factory
 type MonitorFactory = unit -> IMonitor
 
@@ -53,11 +53,11 @@ type OkanshiMonitor private() =
             tagDictionary.Clear()
             value |> Seq.iter (fun x -> tagDictionary.TryAdd(x, byte 0) |> ignore)
     /// Gets the monitor key used to identify monitors
-    static member GetMonitorKey(name : string, monitorType : Type) = (name, monitorType)
+    static member GetMonitorKey(name : string, monitorType : Type, tags : Tag array) = (name, monitorType, tags)
 
     /// Get or add a BasicCounter
     static member BasicCounter(name : string) =
-        let monitorKey = OkanshiMonitor.GetMonitorKey(name, typeof<BasicCounter>)
+        let monitorKey = OkanshiMonitor.GetMonitorKey(name, typeof<BasicCounter>, [||])
         let factory = fun () -> (new BasicCounter(MonitorConfig.Build(name).WithTags(OkanshiMonitor.DefaultTags)) :> IMonitor)
         monitorAgent.PostAndReply(fun reply -> GetMonitor(monitorKey, factory, reply)) :?> BasicCounter
 
@@ -66,7 +66,7 @@ type OkanshiMonitor private() =
 
     /// Get or add a StepCounter
     static member StepCounter(name : string, step) =
-        let monitorKey = OkanshiMonitor.GetMonitorKey(name, typeof<StepCounter>)
+        let monitorKey = OkanshiMonitor.GetMonitorKey(name, typeof<StepCounter>, [||])
         let factory = fun () -> (new StepCounter(MonitorConfig.Build(name).WithTags(OkanshiMonitor.DefaultTags), step) :> IMonitor)
         monitorAgent.PostAndReply(fun reply -> GetMonitor(monitorKey, factory, reply)) :?> StepCounter
 
@@ -75,7 +75,7 @@ type OkanshiMonitor private() =
 
     /// Get or add a PeakRateCounter
     static member PeakRateCounter(name : string, step) =
-        let monitorKey = OkanshiMonitor.GetMonitorKey(name, typeof<PeakRateCounter>)
+        let monitorKey = OkanshiMonitor.GetMonitorKey(name, typeof<PeakRateCounter>, [||])
         let factory = fun () -> (new PeakRateCounter(MonitorConfig.Build(name).WithTags(OkanshiMonitor.DefaultTags), step) :> IMonitor)
         monitorAgent.PostAndReply(fun reply -> GetMonitor(monitorKey, factory, reply)) :?> PeakRateCounter
 
@@ -84,43 +84,43 @@ type OkanshiMonitor private() =
 
     /// Get or add a DoubleCounter
     static member DoubleCounter(name : string, step) =
-        let monitorKey = OkanshiMonitor.GetMonitorKey(name, typeof<DoubleCounter>)
+        let monitorKey = OkanshiMonitor.GetMonitorKey(name, typeof<DoubleCounter>, [||])
         let factory = fun () -> (new DoubleCounter(MonitorConfig.Build(name).WithTags(OkanshiMonitor.DefaultTags), step) :> IMonitor)
         monitorAgent.PostAndReply(fun reply -> GetMonitor(monitorKey, factory, reply)) :?> DoubleCounter
 
     /// Get or add a BasicGauge
     static member BasicGauge<'T>(name : string, getValue : Func<'T>) =
-        let monitorKey = OkanshiMonitor.GetMonitorKey(name, typeof<BasicGauge<'T>>)
+        let monitorKey = OkanshiMonitor.GetMonitorKey(name, typeof<BasicGauge<'T>>, [||])
         let factory = fun () -> (new BasicGauge<'T>(MonitorConfig.Build(name).WithTags(OkanshiMonitor.DefaultTags), getValue)) :> IMonitor
         monitorAgent.PostAndReply(fun reply -> GetMonitor(monitorKey, factory, reply)) :?> BasicGauge<'T>
 
     /// Get or add a MaxGauge
     static member MaxGauge(name : string) =
-        let monitorKey = OkanshiMonitor.GetMonitorKey(name, typeof<MaxGauge>)
+        let monitorKey = OkanshiMonitor.GetMonitorKey(name, typeof<MaxGauge>, [||])
         let factory = fun () -> (new MaxGauge(MonitorConfig.Build(name).WithTags(OkanshiMonitor.DefaultTags))) :> IMonitor
         monitorAgent.PostAndReply(fun reply -> GetMonitor(monitorKey, factory, reply)) :?> MaxGauge
 
     /// Get or add a MinGauge
     static member MinGauge(name : string) =
-        let monitorKey = OkanshiMonitor.GetMonitorKey(name, typeof<MinGauge>)
+        let monitorKey = OkanshiMonitor.GetMonitorKey(name, typeof<MinGauge>, [||])
         let factory = fun () -> (new MaxGauge(MonitorConfig.Build(name).WithTags(OkanshiMonitor.DefaultTags))) :> IMonitor
         monitorAgent.PostAndReply(fun reply -> GetMonitor(monitorKey, factory, reply)) :?> MinGauge
 
     /// Get or add a LongGauge
     static member LongGauge(name : string) =
-        let monitorKey = OkanshiMonitor.GetMonitorKey(name, typeof<LongGauge>)
+        let monitorKey = OkanshiMonitor.GetMonitorKey(name, typeof<LongGauge>, [||])
         let factory = fun () -> (new LongGauge(MonitorConfig.Build(name).WithTags(OkanshiMonitor.DefaultTags))) :> IMonitor
         monitorAgent.PostAndReply(fun reply -> GetMonitor(monitorKey, factory, reply)) :?> LongGauge
 
     /// Get or add a DoubleGauge
     static member DoubleGauge(name : string) =
-        let monitorKey = OkanshiMonitor.GetMonitorKey(name, typeof<DoubleGauge>)
+        let monitorKey = OkanshiMonitor.GetMonitorKey(name, typeof<DoubleGauge>, [||])
         let factory = fun () -> (new DoubleGauge(MonitorConfig.Build(name).WithTags(OkanshiMonitor.DefaultTags))) :> IMonitor
         monitorAgent.PostAndReply(fun reply -> GetMonitor(monitorKey, factory, reply)) :?> DoubleGauge
 
     /// Get or add a DecimalGauge
     static member DecimalGauge(name : string) =
-        let monitorKey = OkanshiMonitor.GetMonitorKey(name, typeof<DecimalGauge>)
+        let monitorKey = OkanshiMonitor.GetMonitorKey(name, typeof<DecimalGauge>, [||])
         let factory = fun () -> (new DecimalGauge(MonitorConfig.Build(name).WithTags(OkanshiMonitor.DefaultTags))) :> IMonitor
         monitorAgent.PostAndReply(fun reply -> GetMonitor(monitorKey, factory, reply)) :?> DecimalGauge
 
@@ -129,6 +129,6 @@ type OkanshiMonitor private() =
 
     /// Get or add a BasicTimer
     static member BasicTimer(name : string, step) =
-        let monitorKey = OkanshiMonitor.GetMonitorKey(name, typeof<BasicTimer>)
+        let monitorKey = OkanshiMonitor.GetMonitorKey(name, typeof<BasicTimer>, [||])
         let factory = fun () -> (new BasicTimer(MonitorConfig.Build(name).WithTags(OkanshiMonitor.DefaultTags), step)) :> IMonitor
         monitorAgent.PostAndReply(fun reply -> GetMonitor(monitorKey, factory, reply)) :?> BasicTimer
