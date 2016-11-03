cc3bdaa95acd3d54bb72942ac4a92b0dd71901b1
Kim Christensen <kich@mvno.dk>
2016-11-03 20:38:43 +0100

    Bump version to 3.1.0

--- src/Okanshi/AssemblyInfo.fs
+++ src/Okanshi/AssemblyInfo.fs
@@ -4,11 +4,11 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("Okanshi")>]
 [<assembly: AssemblyProductAttribute("Okanshi")>]
 [<assembly: AssemblyDescriptionAttribute("In-process monitoring solution")>]
-[<assembly: AssemblyFileVersionAttribute("3.0.0")>]
+[<assembly: AssemblyFileVersionAttribute("3.1.0")>]
 [<assembly: AssemblyVersionAttribute("3.0.0")>]
-[<assembly: AssemblyInformationalVersionAttribute("3.0.0")>]
+[<assembly: AssemblyInformationalVersionAttribute("3.1.0")>]
 do ()
 
 module internal AssemblyVersionInformation =
     let [<Literal>] Version = "3.0.0"
-    let [<Literal>] InformationalVersion = "3.0.0"
+    let [<Literal>] InformationalVersion = "3.1.0"
