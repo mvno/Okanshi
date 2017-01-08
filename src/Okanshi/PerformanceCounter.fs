aa7bb2df3b7289d04b0bf77660fde07cfc6b34ce
Kim Christensen <kich@mvno.dk>
2017-01-08 21:20:33 +0100

    Add PerformanceCounterMonitor
    Allows monitoring Windows performance counters

---
+++ src/Okanshi/PerformanceCounter.fs
@@ -0,0 +1,45 @@
+﻿namespace Okanshi
+
+open System.Diagnostics
+
+/// Performance counter configuration
+type PerformanceCounterConfig = 
+    { /// The category
+      Category : string
+      /// The category
+      Counter : string
+      /// The category
+      Instance : string }
+    
+    /// Create performance counter config without an instance
+    static member Build(category, counter) = 
+        { Category = category
+          Counter = counter
+          Instance = "" }
+    
+    /// Create performance counter config with an instance
+    static member Build(category, counter, instance) = 
+        { Category = category
+          Counter = counter
+          Instance = instance }
+
+/// Used to monitor Windows performance counters. Be aware that some performance counters, requires multiple readings before
+/// returning any value.
+type PerformanceCounterMonitor(registry : IMonitorRegistry, monitorConfig : MonitorConfig, performanceCounterConfig) = 
+    let performanceCounter = 
+        new PerformanceCounter(performanceCounterConfig.Category, performanceCounterConfig.Counter, 
+                               performanceCounterConfig.Instance, true)
+    let gauge = new BasicGauge<_>(monitorConfig, fun () -> performanceCounter.NextValue())
+    do registry.Register(gauge)
+    new(monitorConfig, performanceCounterConfig) = 
+        PerformanceCounterMonitor(DefaultMonitorRegistry.Instance, monitorConfig, performanceCounterConfig)
+    
+    /// Gets the performance counter value
+    member __.GetValue() = gauge.GetValue()
+    
+    /// Gets the monitor config
+    member __.Config = gauge.Config
+    
+    interface IMonitor with
+        member self.GetValue() = self.GetValue() :> obj
+        member self.Config = self.Config
---
+++ tests/Okanshi.Tests/PerformanceCounterTest.cs
@@ -0,0 +1,39 @@
+﻿using System.Diagnostics;
+using FluentAssertions;
+using Xunit;
+
+namespace Okanshi.Test
+{
+	public class PerformanceCounterTest
+	{
+		[Fact]
+		public void Performance_counter_without_instance_name()
+		{
+			var performanceCounter = new PerformanceCounter("Memory", "Available Bytes");
+
+			var monitor = new PerformanceCounterMonitor(MonitorConfig.Build("Test"),
+				PerformanceCounterConfig.Build("Memory", "Available Bytes"));
+
+			monitor.GetValue()
+				.Should()
+				.BeGreaterThan(0)
+				.And.BeApproximately(performanceCounter.NextValue(), 100000,
+					"Because memory usage can change between the two values");
+		}
+
+		[Fact]
+		public void Performance_counter_with_instance_name()
+		{
+			var performanceCounter = new PerformanceCounter("Process", "Private Bytes", Process.GetCurrentProcess().ProcessName);
+
+			var monitor = new PerformanceCounterMonitor(MonitorConfig.Build("Test"),
+				PerformanceCounterConfig.Build("Process", "Private Bytes", Process.GetCurrentProcess().ProcessName));
+
+			monitor.GetValue()
+				.Should()
+				.BeGreaterThan(0)
+				.And.BeApproximately(performanceCounter.NextValue(), 100000,
+					"Because memory usage can change between the two values");
+		}
+	}
+}
