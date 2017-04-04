de95de8c5267af3e12d331142f6226ba5cec3df2
Kim Christensen <kimworking@gmail.com>
2017-04-04 14:17:17 +0200

    Add default values to options

--- Okanshi.InfluxDBObserver/InfluxDbObserverOptions.cs
+++ Okanshi.InfluxDBObserver/InfluxDbObserverOptions.cs
@@ -1,6 +1,6 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Okanshi.Observers {
     public class InfluxDbObserverOptions {
@@ -8,9 +8,9 @@ namespace Okanshi.Observers {
 
         public string RetentionPolicy { get; set; }
 
-        public Func<Tag, bool> TagToFieldSelector { get; set; }
+        public Func<Tag, bool> TagToFieldSelector { get; set; } = x => false;
 
-        public IEnumerable<string> TagsToIgnore { get; set; }
+        public IEnumerable<string> TagsToIgnore { get; set; } = Enumerable.Empty<string>();
 
         public InfluxDbObserverOptions(string databaseName) {
             DatabaseName = databaseName;
