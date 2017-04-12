10a5e25d29836c54773bf81476b472dc6492366d
Kim Christensen <kimworking@gmail.com>
2017-04-12 10:30:50 +0200

    Add AppBuilder extension

---
+++ Okanshi.Owin/AppBuilderExtensions.cs
@@ -0,0 +1,10 @@
+﻿using Owin;
+
+namespace Okanshi.Owin {
+    public static class AppBuilderExtensions {
+        public static void UseOkanshi(this IAppBuilder appBuilder, OkanshiOwinOptions options = null)
+        {
+            appBuilder.Use(typeof(OkanshiMiddleware), options ?? new OkanshiOwinOptions());
+        }
+    }
+}
