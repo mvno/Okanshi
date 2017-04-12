4b77fd2311da9e678abb5f264b1afe4d1f64e997
Kim Christensen <kimworking@gmail.com>
2017-04-12 10:31:10 +0200

    Implement sample project

--- samples/Owin/Program.cs
+++ samples/Owin/Program.cs
@@ -1,7 +1,16 @@
-﻿namespace Owin {
-    public class Program {
-        static void Main(string[] args) {
+﻿using System;
+using Microsoft.Owin.Hosting;
 
+namespace Owin
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            const string baseAddress = "http://localhost:9000/";
+            WebApp.Start(baseAddress);
+            Console.WriteLine("Press ENTER to exit program...");
+            Console.ReadLine();
         }
     }
 }
