6e5682f5737d728e19b10c3979e2bf3e56c03d1c
Kim Christensen <kimworking@gmail.com>
2017-05-16 12:44:48 +0200

    Reformat clock.fs

--- src/Okanshi/Clock.fs
+++ src/Okanshi/Clock.fs
@@ -3,85 +3,91 @@ namespace Okanshi
 open System
 
 /// Interface for defining the clock used
-type IClock =
+type IClock = 
+    
     /// Gets the current time
-    abstract member Now : unit -> DateTime
+    abstract Now : unit -> DateTime
+    
     /// Gets the current ticks
-    abstract member NowTicks : unit -> int64
+    abstract NowTicks : unit -> int64
+    
     /// Freezes the time. Used to make sure composite metrics go into the same
     /// buckets across atomics
-    abstract member Freeze : unit -> unit
+    abstract Freeze : unit -> unit
+    
     /// Unfreezes the time
-    abstract member Unfreeze : unit -> unit
+    abstract Unfreeze : unit -> unit
 
 /// Wrapper around the system time
-type SystemClock() =
+type SystemClock() = 
     static let instance = new SystemClock()
     let mutable frozenAt : DateTime option = None
-
     let freeze() = frozenAt <- Some(DateTime.UtcNow)
     let unfreeze() = frozenAt <- None
     
-    let now() =
+    let now() = 
         match frozenAt with
         | Some(x) -> x
         | None -> DateTime.UtcNow
-
+    
     /// Gets the current time
-    member __.Now() =
+    member __.Now() = 
         match frozenAt with
         | Some(x) -> x
         | None -> DateTime.UtcNow
     
     /// Gets the current ticks
     member self.NowTicks() = self.Now().Ticks
-
+    
     /// Freezes the time. Used to make sure composite metrics go into the same
     /// buckets across atomics
-    member __.Freeze() =
-        frozenAt <- Some(DateTime.UtcNow)
-
+    member __.Freeze() = frozenAt <- Some(DateTime.UtcNow)
+    
     /// Unfreezes the time
-    member __.Unfreeze() =
-        frozenAt <- None
-
+    member __.Unfreeze() = frozenAt <- None
+    
     static member Instance = instance
-
     interface IClock with
+        
         /// Gets the current time
         member self.Now() = self.Now()
+        
         /// Gets the current ticks
         member self.NowTicks() = self.NowTicks()
+        
         /// Freezes the time. Used to make sure composite metrics go into the same
         /// buckets across atomics
         member self.Freeze() = ()
+        
         /// Unfreezes the time
         member self.Unfreeze() = ()
 
 /// Manual clock, should only be used in tests
-type ManualClock() =
+type ManualClock() = 
     let mutable currentTime = DateTime.UtcNow
-
+    
     /// Gets the current time
     member __.Now() = currentTime
-
+    
     /// Gets the current ticks
     member __.NowTicks() = currentTime.Ticks
-
+    
     /// Sets time current time
-    member __.Set(time : DateTime) =
-        currentTime <- time
-
+    member __.Set(time : DateTime) = currentTime <- time
+    
     /// Advance the time
-    member __.Advance(span : TimeSpan) =
-        currentTime <- currentTime.Add(span)
-
+    member __.Advance(span : TimeSpan) = currentTime <- currentTime.Add(span)
+    
     interface IClock with
+        
         /// Gets the current time
         member self.Now() = self.Now()
+        
         /// Gets the current ticks
         member self.NowTicks() = self.NowTicks()
+        
         /// Does nothing in ManualClock, as time is always frozen
         member self.Freeze() = ()
+        
         /// Does nothing in ManualClock, as time is always frozen
         member self.Unfreeze() = ()
