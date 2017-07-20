82fd832f53228e462f2483a9ff58bc35a4633118
Kim Christensen <kimworking@gmail.com>
2017-07-20 11:20:43 +0200

    Bump version to 4.0.4

--- src/Okanshi.Owin/Properties/AssemblyInfo.cs
+++ src/Okanshi.Owin/Properties/AssemblyInfo.cs
@@ -4,16 +4,16 @@ using System.Reflection;
 [assembly: AssemblyTitleAttribute("Okanshi.Owin")]
 [assembly: AssemblyProductAttribute("Okanshi")]
 [assembly: AssemblyDescriptionAttribute("In-process monitoring solution")]
-[assembly: AssemblyFileVersionAttribute("4.0.3")]
+[assembly: AssemblyFileVersionAttribute("4.0.4")]
 [assembly: AssemblyVersionAttribute("4.0.0")]
-[assembly: AssemblyInformationalVersionAttribute("4.0.3")]
+[assembly: AssemblyInformationalVersionAttribute("4.0.4")]
 namespace System {
     internal static class AssemblyVersionInformation {
         internal const System.String AssemblyTitle = "Okanshi.Owin";
         internal const System.String AssemblyProduct = "Okanshi";
         internal const System.String AssemblyDescription = "In-process monitoring solution";
-        internal const System.String AssemblyFileVersion = "4.0.3";
+        internal const System.String AssemblyFileVersion = "4.0.4";
         internal const System.String AssemblyVersion = "4.0.0";
-        internal const System.String AssemblyInformationalVersion = "4.0.3";
+        internal const System.String AssemblyInformationalVersion = "4.0.4";
     }
 }
--- src/Okanshi/AssemblyInfo.fs
+++ src/Okanshi/AssemblyInfo.fs
@@ -5,15 +5,15 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("Okanshi")>]
 [<assembly: AssemblyProductAttribute("Okanshi")>]
 [<assembly: AssemblyDescriptionAttribute("In-process monitoring solution")>]
-[<assembly: AssemblyFileVersionAttribute("4.0.3")>]
+[<assembly: AssemblyFileVersionAttribute("4.0.4")>]
 [<assembly: AssemblyVersionAttribute("4.0.0")>]
-[<assembly: AssemblyInformationalVersionAttribute("4.0.3")>]
+[<assembly: AssemblyInformationalVersionAttribute("4.0.4")>]
 do ()
 
 module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyTitle = "Okanshi"
     let [<Literal>] AssemblyProduct = "Okanshi"
     let [<Literal>] AssemblyDescription = "In-process monitoring solution"
-    let [<Literal>] AssemblyFileVersion = "4.0.3"
+    let [<Literal>] AssemblyFileVersion = "4.0.4"
     let [<Literal>] AssemblyVersion = "4.0.0"
-    let [<Literal>] AssemblyInformationalVersion = "4.0.3"
+    let [<Literal>] AssemblyInformationalVersion = "4.0.4"
