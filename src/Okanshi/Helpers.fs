243bd710526afab0bec1d91a3b2f3e40af17c6af
Kim Christensen <kimworking@gmail.com>
2017-07-20 11:09:10 +0200

    Fix lock bug

--- src/Okanshi/Helpers.fs
+++ src/Okanshi/Helpers.fs
@@ -5,9 +5,8 @@ module Lock =
     let lock = lock
     
     let inline lockWithArg (lockObj : 'T when 'T : not struct) arg f = 
-        let mutable lockTaken = false
         try 
-            System.Threading.Monitor.Enter(lockObj, ref lockTaken)
+            System.Threading.Monitor.Enter(lockObj)
             f arg
         finally
-            if lockTaken then System.Threading.Monitor.Exit(lockObj)
+            System.Threading.Monitor.Exit(lockObj)
