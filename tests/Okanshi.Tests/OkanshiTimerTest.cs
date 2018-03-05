82121eed9d5a38be939f82ec102f62f0164eb055
Kim Christensen <kimworking@gmail.com>
2018-03-05 09:40:22 +0100

    Convert OkanshiTimer test to new stopwatch

--- tests/Okanshi.Tests/OkanshiTimerTest.cs
+++ tests/Okanshi.Tests/OkanshiTimerTest.cs
@@ -2,19 +2,24 @@ using System;
 using System.Threading;
 using FluentAssertions;
 using Xunit;
+using NSubstitute;
 
 namespace Okanshi.Test
 {
     public class OkanshiTimerTest
     {
-        private readonly OkanshiTimer _timer = new OkanshiTimer(x => { });
+        private readonly IStopwatch _stopwatch = Substitute.For<IStopwatch>();
+        private readonly OkanshiTimer _timer;
 
-        public OkanshiTimerTest() { }
+        public OkanshiTimerTest() {
+            _timer = new OkanshiTimer(x => {}, () => _stopwatch);
+        }
 
         [Fact]
         public void Cannot_be_started_multiple_times()
         {
             _timer.Start();
+            _stopwatch.IsRunning.Returns(true);
 
             Action start = () => _timer.Start();
 
@@ -25,7 +30,9 @@ namespace Okanshi.Test
         public void Cannot_be_stopped_multiple_times()
         {
             _timer.Start();
+            _stopwatch.IsRunning.Returns(true);
             _timer.Stop();
+            _stopwatch.IsRunning.Returns(false);
 
             Action stop = () => _timer.Stop();
 
@@ -44,7 +51,9 @@ namespace Okanshi.Test
         public void Cannot_be_started_if_already_used_once()
         {
             _timer.Start();
+            _stopwatch.IsRunning.Returns(true);
             _timer.Stop();
+            _stopwatch.IsRunning.Returns(false);
 
             Action start = () => _timer.Start();
 
