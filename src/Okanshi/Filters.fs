ed552db5bbb4aa20623e0133c9f6af640c0c8bfb
Kasper B. Graversen <kbilsted@users.noreply.github.com>
2018-10-28 20:05:42 +0100

    Add monitor filters

---
+++ src/Okanshi/Filters.fs
@@ -0,0 +1,132 @@
+﻿namespace Okanshi
+
+open System
+open System.Diagnostics
+
+/// A filter decorator for gauges.
+/// The wrapped monitor only returns values to the poller in case a change has been registered
+/// With this you can avoid sending 0-value measurements that normally is sent when no measurements are registered
+type GaugeZeroFilter<'T>(inner : IGauge<'T>) =
+    let mutable hasChanged = false
+    let syncRoot = new obj()
+
+    member __.Set(newValue) = 
+        lock syncRoot (fun() -> 
+            hasChanged <- true
+            inner.Set(newValue))
+    member __.GetValues() = 
+        lock syncRoot (fun() -> 
+            match hasChanged with
+            | false -> Seq.empty<IMeasurement>
+            | true -> 
+                hasChanged <- false
+                inner.GetValues() |> Seq.cast)
+    member __.Config = inner.Config
+    member __.Reset() = 
+        lock syncRoot (fun() ->
+            hasChanged <- true
+            inner.Reset())
+    member __.GetValuesAndReset() = 
+        lock syncRoot (fun() -> 
+            match hasChanged with
+            | false -> Seq.empty<IMeasurement>
+            | true -> 
+                hasChanged <- false
+                inner.GetValuesAndReset() |> Seq.cast)
+
+    interface IGauge<'T> with
+        member self.Set(newValue) = self.Set(newValue)
+        member self.GetValues() = self.GetValues()
+        member self.Config = self.Config
+        member self.Reset() = self.Reset()
+        member self.GetValuesAndReset() = self.GetValuesAndReset()
+
+
+/// A filter decorator for counters.
+/// The wrapped monitor only returns values to the poller in case a change has been registered
+/// With this you can avoid sending 0-value measurements that normally is sent when no measurements are registered
+type CounterZeroFilter<'T>(inner : ICounter<'T>) =
+    let mutable hasChanged = false
+    let syncRoot = new obj()
+    
+    member __.Increment() = 
+        lock syncRoot (fun() -> 
+            hasChanged <- true
+            inner.Increment())
+    member __.Increment(amount) = 
+        lock syncRoot (fun() -> 
+            hasChanged <- true
+            inner.Increment(amount))
+    member __.GetValues() =
+        lock syncRoot (fun() -> 
+            match hasChanged with
+            | false -> Seq.empty<IMeasurement>
+            | true -> 
+                hasChanged <- false
+                inner.GetValues() |> Seq.cast)
+    member __.Config = inner.Config
+    member __.GetValuesAndReset() = 
+        lock syncRoot (fun() -> 
+            match hasChanged with
+            | false -> Seq.empty<IMeasurement>
+            | true -> 
+                hasChanged <- false
+                inner.GetValuesAndReset() |> Seq.cast)
+
+    interface ICounter<'T> with
+        member self.Increment() = self.Increment()
+        member self.Increment(amount) = self.Increment(amount)
+        member self.GetValues() = self.GetValues()
+        member self.Config = self.Config
+        member self.GetValuesAndReset() = self.GetValuesAndReset()
+
+
+/// A filter decorator for timers.
+/// The wrapped monitor only returns values to the poller in case a change has been registered
+/// With this you can avoid sending 0-value measurements that normally is sent when no measurements are registered
+type TimerZeroFilter(inner : ITimer) =
+    let mutable hasChanged = false
+    let syncRoot = new obj()
+    
+    member __.Record(f : Func<'T>) = 
+        lock syncRoot (fun() -> 
+            hasChanged <- true
+            inner.Record(f))
+    member __.Record(f : Action) = 
+        lock syncRoot (fun() -> 
+            hasChanged <- true
+            inner.Record(f))
+    member __.GetValues() = 
+        lock syncRoot (fun() -> 
+            match hasChanged with
+            | false -> Seq.empty<IMeasurement>
+            | true -> 
+                hasChanged <- false
+                inner.GetValues() |> Seq.cast)
+    member __.Config = inner.Config
+    member __.Start() = inner.Start()
+    member __.RegisterElapsed(elapsed : Stopwatch) = 
+        lock syncRoot (fun() -> 
+            hasChanged <- true
+            inner.RegisterElapsed(elapsed))
+    member __.Register(elapsed : TimeSpan) = 
+        lock syncRoot (fun() -> 
+            hasChanged <- true
+            inner.Register(elapsed))
+    member __.GetValuesAndReset() = 
+        lock syncRoot (fun() -> 
+            match hasChanged with
+            | false -> Seq.empty<IMeasurement>
+            | true -> 
+                hasChanged <- false
+                inner.GetValuesAndReset() |> Seq.cast)
+
+    interface ITimer with
+        member self.Record(f : Func<'T>) = self.Record(f)
+        member self.Record(f : Action) = self.Record(f)
+        member self.GetValues() = self.GetValues()
+        member self.Config = self.Config
+        member self.Start() = self.Start()
+        member self.RegisterElapsed(elapsed : Stopwatch) = self.RegisterElapsed(elapsed)
+        member self.Register(elapsed : TimeSpan) = self.Register(elapsed)
+        member self.GetValuesAndReset() = self.GetValuesAndReset()
---
+++ tests/Okanshi.Tests/FilterGaugeTest.cs
@@ -0,0 +1,88 @@
+using System.Linq;
+using FluentAssertions;
+using Xunit;
+
+namespace Okanshi.Test
+{
+    public class FilterGaugeTest
+    {
+        [Fact]
+        public void FilterGauge_is_an_igauge()
+        {
+            IGauge<long> gauge = new GaugeZeroFilter<long>(new LongGauge(MonitorConfig.Build("Test")));
+        }
+
+        [Fact]
+        public void Do_not_send_data_when_nothing_registered()
+        {
+            var gauge = new GaugeZeroFilter<long>(new LongGauge(MonitorConfig.Build("Test")));
+            
+            gauge.GetValues().ShouldBeEquivalentTo(new IMeasurement[0]);
+            ((IGauge<long>)gauge).GetValues().ShouldBeEquivalentTo(new IMeasurement[0]);
+        }
+
+        [Fact]
+        public void Do_not_send_data_when_nothing_registered_after_GetValues()
+        {
+            var gauge = new GaugeZeroFilter<long>(new LongGauge(MonitorConfig.Build("Test")));
+            gauge.Set(33);
+            gauge.GetValues();
+            
+            gauge.GetValues().ShouldBeEquivalentTo(new IMeasurement[0]);
+            ((IGauge<long>)gauge).GetValues().ShouldBeEquivalentTo(new IMeasurement[0]);
+        }
+
+        [Theory]
+        [InlineData(-3)]
+        [InlineData(0)]
+        [InlineData(42)]        
+        public void Send_data_when_registered(int someValue)
+        {
+            var gauge = new GaugeZeroFilter<long>(new LongGauge(MonitorConfig.Build("Test")));
+            gauge.Set(someValue);
+
+            gauge.GetValues().Single();
+        }
+
+        [Fact]
+        public void Send_data_when_Reset()
+        {
+            var gauge = new GaugeZeroFilter<long>(new LongGauge(MonitorConfig.Build("Test")));
+            gauge.Reset();
+
+            gauge.GetValues().Single();
+        }
+
+        [Fact]
+        public void Do_not_send_data_and_reset_when_nothing_registered()
+        {
+            var gauge = new GaugeZeroFilter<long>(new LongGauge(MonitorConfig.Build("Test")));
+            
+            gauge.GetValuesAndReset().ShouldBeEquivalentTo(new IMeasurement[0]);
+            ((IGauge<long>)gauge).GetValuesAndReset().ShouldBeEquivalentTo(new IMeasurement[0]);
+        }
+
+        [Theory]
+        [InlineData(-3)]
+        [InlineData(0)]
+        [InlineData(42)]
+        public void Do_not_send_data_and_reset_when_nothing_registered_after_GetValues(int someValue)
+        {
+            var gauge = new GaugeZeroFilter<long>(new LongGauge(MonitorConfig.Build("Test")));
+            gauge.Set(someValue);
+            gauge.GetValuesAndReset();
+            
+            gauge.GetValues().ShouldBeEquivalentTo(new IMeasurement[0]);
+            ((IGauge<long>)gauge).GetValuesAndReset().ShouldBeEquivalentTo(new IMeasurement[0]);
+        }
+
+        [Fact]
+        public void Send_data_and_reset_when_registered()
+        {
+            var gauge = new GaugeZeroFilter<long>(new LongGauge(MonitorConfig.Build("Test")));
+            gauge.Set(33);
+
+            gauge.GetValuesAndReset().Single();
+        }
+    }
+}
