6e42d2b93eebd161d2249d7efaf901fb65cc59af
Kasper B. Graversen <kbilsted@users.noreply.github.com>
2019-02-27 20:19:12 +0100

    fix idention

--- src/Okanshi.WebApi/OkanshiWebApiOptions.cs
+++ src/Okanshi.WebApi/OkanshiWebApiOptions.cs
@@ -2,37 +2,37 @@
 
 namespace Okanshi.WebApi
 {
-	/// <summary>
-	/// Okanshi options
-	/// </summary>
-	public class OkanshiWebApiOptions
-	{
-		/// <summary>
-		/// Should status codes be added as tags. Default value is true.
-		/// </summary>
-		public bool AddStatusCodeTag { get; set; } = true;
+    /// <summary>
+    /// Okanshi options
+    /// </summary>
+    public class OkanshiWebApiOptions
+    {
+        /// <summary>
+        /// Should status codes be added as tags. Default value is true.
+        /// </summary>
+        public bool AddStatusCodeTag { get; set; } = true;
 
-		/// <summary>
-		/// The metric name to use. Default values is Request.
-		/// </summary>
-		public string MetricName { get; set; } = "Request";
+        /// <summary>
+        /// The metric name to use. Default values is Request.
+        /// </summary>
+        public string MetricName { get; set; } = "Request";
 
-		/// <summary>
-		/// How the request path is extracted
-		/// </summary>
-		public RequestPathExtraction PathExtraction { get; set; } = RequestPathExtraction.Path;
+        /// <summary>
+        /// How the request path is extracted
+        /// </summary>
+        public RequestPathExtraction PathExtraction { get; set; } = RequestPathExtraction.Path;
 
-		/// <summary>
-		/// A factory method which is invoked whenever a timer is needed
-		/// </summary>
-		public Func<string, Tag[], ITimer> TimerFactory = (name, tags) => OkanshiMonitor.Timer(name, tags);
-	}
+        /// <summary>
+        /// A factory method which is invoked whenever a timer is needed
+        /// </summary>
+        public Func<string, Tag[], ITimer> TimerFactory = (name, tags) => OkanshiMonitor.Timer(name, tags);
+    }
 
-	/// <summary>
+    /// <summary>
     /// How the request path is extracted
     /// </summary>
-	public enum RequestPathExtraction
-	{
+    public enum RequestPathExtraction
+    {
         /// <summary>
         /// Uses "request.RequestUri.LocalPath" which holds the path including any parameters within the path, e.g. customerId
         /// </summary>
@@ -45,5 +45,5 @@ namespace Okanshi.WebApi
         /// this code will use an abstract path such as "api/{controller}/{id}" which will be the general configured fall-back path.
         /// </summary>
         CanonicalPath,
-	}
+    }
 }
